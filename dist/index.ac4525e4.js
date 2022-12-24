9f8f641a9ac9cc17ac8c76c529d554fd53b34f69
Dr_Birkin <57367079+drbirkin@users.noreply.github.com>
2022-12-24 03:42:19 +0100

    Delete dist directory

--- dist/index.ac4525e4.js
+++
@@ -1,2 +0,0 @@
-"use strict";window.onscroll=function(){var e=document.querySelector(".navbar-area"),o=e.offsetTop;window.pageYOffset>o?e.classList.add("sticky"):e.classList.remove("sticky");var s=document.querySelector(".scroll-top");document.body.scrollTop>50||document.documentElement.scrollTop>50?s.style.display="flex":s.style.display="none"};
-//# sourceMappingURL=index.ac4525e4.js.map
